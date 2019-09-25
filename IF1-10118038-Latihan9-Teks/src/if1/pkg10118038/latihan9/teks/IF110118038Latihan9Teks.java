adceb0e167799ff9907f43098e38ef259ded7b5c
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:41:15 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan9-Teks/src/if1/pkg10118038/latihan9/teks/IF110118038Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan9.teks;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan
+
+ */
+public class IF110118038Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c );
+    }
+    
+}
